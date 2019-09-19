94944f20f3d90db99f77e3a728b932539ce52a70
jfcarocota <jfcarocota@gmail.com>
2019-09-19 08:41:58 -0700

    play director

---
+++ Assets/Scripts/PlayTimeline.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Timeline;
+using UnityEngine.Playables;
+
+public class PlayTimeline : MonoBehaviour
+{
+    Button btnPlayTimeline;
+    [SerializeField]
+    PlayableDirector playableDirector;
+
+    void Awake()
+    {
+        btnPlayTimeline = GetComponent<Button>();
+        btnPlayTimeline.onClick.AddListener(ButtonPlayTimeline);
+    }
+
+    void ButtonPlayTimeline()
+    {
+        Debug.Log("Play timeline");
+        playableDirector.Play();
+    }
+}
