49ce974433b106906468a2fef6fa21b400f19719
jfcarocota <jfcarocota@gmail.com>
2019-09-23 07:52:29 -0700

    pause and stop added

---
+++ Assets/Scripts/TimelineControls.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Timeline;
+using UnityEngine.Playables;
+
+public class TimelineControls : MonoBehaviour
+{
+    [SerializeField]
+    PlayableDirector playableDirector;
+
+    [SerializeField]
+    Button btnPlayTimeline;
+    [SerializeField]
+    Button btnPauseTimeline;
+    [SerializeField]
+    Button btnStopTimeline;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        btnPlayTimeline.onClick.AddListener(PlayTimeline);
+        btnPauseTimeline.onClick.AddListener(PauseTimeline);
+        btnStopTimeline.onClick.AddListener(StopTimeline);
+    }
+
+    void PlayTimeline()
+    {
+        playableDirector.Play();
+    }
+
+    void PauseTimeline()
+    {
+        playableDirector.Pause();
+    }
+
+    void StopTimeline()
+    {
+        playableDirector.Stop();
+    }
+}
